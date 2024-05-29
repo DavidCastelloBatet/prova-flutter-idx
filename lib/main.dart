3becc47c40a896fda1c06be7d335b6f864919622
davidcastellobatetdev <davidcastellobatetdev@gmail.com>
2024-05-29 14:55:17 +0000

    Modificacio al title

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(useMaterial3: true),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Bentornats a Flutter de nou'),
+          title: const Text('Bentornats a Flutter de nou!'),
           centerTitle: true,
         ),
         body: Container(
